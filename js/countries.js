0106b517f1e0b76f71ef31c1215139813b72d18d
Enamul <muhammad.enamul.haque79@gmail.com>
2023-03-19 12:46:22 +0600

    updated

--- js/countries.js
+++ js/countries.js
@@ -1,3 +1,4 @@
+//function-1:loadCountries data
 const loadCountries =()=>{
     fetch('https://restcountries.com/v3.1/all')
         .then(response => response.json())
@@ -5,27 +6,31 @@ const loadCountries =()=>{
         // .then(data => console.log(data));
 }
 
-//all countries data retrieve function
+//function-2:all countries data retrieve function
+
 const displayCountries =(countries)=>{
-    // console.log(countries);
+    console.log(countries[0]);
     const allCountriesHTML = countries.map(country =>getCountryHTML(country));
-    console.log(allCountriesHTML[0]);
+    // console.log(allCountriesHTML[0]);
     const countriesContainer = document.getElementById('countries');
     countriesContainer.innerHTML = allCountriesHTML.join(" ");
     //to remove coma after anything use .join('empty string');
 };
 
-//single country data retrieve function;
+//function-3:single country data retrieve function;
+
 const getCountryHTML =(country)=>{
     return `
-        <div>
+        <div class="country">
         <h2>${country.name.common}</h2>
         <img src="${country.flags.png}" alt="" />
         <h2>${country.capital}</h2>
-
         </div>
     `;
    
 }
+        
+        
 
-loadCountries();
+
+loadCountries();//this function globally declare;
